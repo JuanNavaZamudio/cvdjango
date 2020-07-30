f38232dcb1f24f153c6cc0cf76ecf70995e7a88a
Juan M. Nava Z <juanmnzsk8@gmail.com>
2020-07-29 20:42:23 -0500

    se agrega configuración para postgres

--- cv/settings.py
+++ cv/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'cv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DATABASES',
+        'USER': 'djangocv',
+        'PASSWORD': 'test159',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
